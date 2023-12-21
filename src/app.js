78baf263b355973e5962d8afc974d7ff47e2e5d3
maxjgarcia <garcia.maximiliano.jesus@gmail.com>
2023-12-21 00:59:14 -0300

    segunda entrega

--- src/app.js
+++ src/app.js
@@ -1,18 +1,28 @@
 import express from "express"
+import handlebars from "express-handlebars"
 import router from "./router/index.js"
+import port from "./configs/server.config.js"
+
 
 const app = express()
 
 app.use(express.json())
+app.use(express.static(process.cwd() + '/src/public'))
+app.engine('handlebars', handlebars.engine())
+app.set('views', process.cwd() + '/src/views')
+app.set('view engine', 'handlebars')
 
 router(app)
 
 app.get("/", (req, res) => {
-  res.status(404).json({ error: "Use the routes '/api/products' or '/api/carts'" });
+  res.render('index', { title: 'Challenge05', style: 'index.css' })
 });
 
 app.get('*', (req, res) => {
   res.status(404).json({ error: 'Not Found 404' })
 })
 
-export default app
+const server = app.listen(port, () => {
+  console.log(`Server runing at http://localhost:${port}`)
+})
+server.on('error', (err) => console.log(`Server Error: ${err}`))
